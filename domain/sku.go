1be4cb833770aabd4a269c3b6b5e7a2b1bf3ce8f
Nicolas <nicolas.senac15@gmail.com>
2020-10-22 23:30:44 -0300

    creating sku

--- domain/sku.go
+++ domain/sku.go
@@ -1,8 +1,22 @@
 package domain
 
+import "github.com/beevik/guid"
+
 //Sku product variation
 type Sku struct {
+	ID       string  `bson:"id" json:"id"`
 	Name     string  `bson:"name" json:"name"`
 	Price    float64 `bson:"price" json:"price"`
-	Quantity float64 `bson:"quantity" json:"quantity"`
+	Quantity int     `bson:"quantity" json:"quantity"`
+}
+
+//NewSku constructor
+func NewSku(name string, price string, quantity int) Sku {
+	newguid := guid.New()
+
+	return Sku{
+		ID:       newguid.String(),
+		Name:     name,
+		Quantity: quantity,
+	}
 }
